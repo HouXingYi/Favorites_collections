8f256064203ae8fa3be1e48e1d9faa3675920a64
houxingyi <1026043046@qq.com>
2017-11-19 11:35:47 +0800

    客户端环境

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+
+    <abc></abc>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+@import "./style/index";
+</style>
