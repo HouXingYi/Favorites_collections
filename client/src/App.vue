d3b3c9fb04833a870541af663ed5280546b3d399
houxingyi <1026043046@qq.com>
2017-11-20 20:32:55 +0800

    change rep name

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-
-    <abc></abc>
-
+    <div class="header"></div>
+    <div class="leftBar"></div>    
+    <div class="mainContainer">
+      main
+    </div> 
   </div>
 </template>
 
@@ -14,4 +16,30 @@ export default {
 
 <style lang="scss">
 @import "./style/index";
+
+#app{
+
+  .header{
+    height: 60px;
+    background: #23262E;
+  }
+  .leftBar{
+    position: fixed;
+    width: 200px;
+    border-right: 1px solid #e5e5e5;
+    background: #123456;
+    top: 60px;
+    left: 0;
+    bottom: 0;
+  }
+  .mainContainer{
+    position: absolute;
+    background: lightcoral;
+    top: 60px;
+    left: 200px;
+    right: 0;
+    bottom: 0;
+  }
+}
+
 </style>
